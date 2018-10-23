c6ca24dd7b276b3c262cb3ac0e4064e630a48fdb
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-10-23 20:08:23 +0300

    Added custom pages for both 403 and 404 errors

--- server.py
+++ server.py
@@ -86,5 +86,15 @@ def edit_post(post_path):
     )
 
 
+@app.errorhandler(403)
+def forbidden(error):
+    return render_template('403.html'), 403
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run()
